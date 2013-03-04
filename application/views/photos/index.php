71045355527ea099e1da255447c9e2a270334122
William Gallios <williamgallios@gmail.com>
2013-03-03 20:35:16 -0800

    added jqueryui 1.101 and ckfinder 2.3.1

--- application/views/photos/index.php
+++ application/views/photos/index.php
@@ -9,6 +9,27 @@
     <div class='span9'>
         <h1>Photos</h1>
 
+<div class='tabbable'>
+    <ul class='nav nav-tabs'>
+        <li class='active'><a href='#tabAlbums' data-toggle="tab">Albums</a></li>
+        <li><a href='#tabUpload' data-toggle="tab">Upload</a></li>
+    </ul>
+
+<div class="tab-content">
+
+    <div id="tabAlbum" class='tab-pane active'>
+    </div> <!-- #tabAlbum //-->
+
+    <div id="tabUpload" class='tab-pane'>
+    </div> <!-- #tabUpload //-->
+
+</div> <!-- .tab-content //-->
+
+</div> <!-- .tabbable //-->
+
+
+
+
 
     </div> <!-- .span9 //-->
 
