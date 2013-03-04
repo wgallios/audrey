da8ba16c75de29cacccf4b4b0df967f15f1ce2ce
William Gallios <williamgallios@gmail.com>
2013-03-03 17:38:53 -0800

    implementing google analytics

--- application/views/templates/headinclude.php
+++ application/views/templates/headinclude.php
@@ -44,3 +44,18 @@
 
     <script type='text/javascript' src="/min/?f=public/js/global.js<?=$this->config->item('min_debug')?>&amp;<?=$this->config->item('min_version')?>"></script>
 
+
+<script type="text/javascript">
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-38992311-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+</script>
+
